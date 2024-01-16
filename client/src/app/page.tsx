f74f5e8219b153453b233448ce2f225d7ae6ed36
Mayk <maycon.lrgo@gmail.com>
2024-01-16 17:17:02 -0300

    iniciando front instagram

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+import '../styles/instagram.css';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Arkadia</h1>
+      <section className="container">
+        <p>Link 1</p>
+        <p>Link 2</p>
+        <p>Link 3</p>
+      </section>
+    </main>
+  )
+}
